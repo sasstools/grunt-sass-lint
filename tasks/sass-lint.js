c149e525bcff1072eabe02ec82d9f2940e42eb1e
Micah Godbolt <mgodbolt@phase2technology.com>
2015-08-27 20:50:52 -0700

    Removed unnecessary variables

--- tasks/sass-lint.js
+++ tasks/sass-lint.js
@@ -5,14 +5,9 @@ var lint = require('sass-lint');
 module.exports = function (grunt) {
 	grunt.verbose.writeln('\n' + lint.info + '\n');
 
-	grunt.registerMultiTask('sasslint', 'Compile Sass to CSS', function () {
+	grunt.registerMultiTask('sasslint', 'Lint your Sass', function () {
 
-			var done = this.async(),
-		      files = this.filesSrc,
-		      fileCount = this.filesSrc.length,
-		      target = this.target,
-		      opts,
-		      message;
+			var opts;
 
       var results = lint.lintFiles(this.data[0], opts);
 
