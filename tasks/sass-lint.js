587aad906a54b27edff5fb1fba61bc957a104af2
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2015-12-12 20:40:23 +0100

    print output on fail linter

--- tasks/sass-lint.js
+++ tasks/sass-lint.js
@@ -1,24 +1,22 @@
 'use strict';
 var path = require('path'),
-		lint = require('sass-lint');
+	lint = require('sass-lint');
 
 module.exports = function (grunt) {
 	grunt.verbose.writeln('\n' + lint.info + '\n');
 
 	grunt.registerMultiTask('sasslint', 'Lint your Sass', function () {
 		var opts = this.options({
-					configFile: ''
-				}),
-				results = lint.lintFiles(this.data[0], opts, opts.configFile),
-				failResultCount = lint.resultCount(results),
-				resultFormat = lint.format(results);
+				configFile: ''
+			}),
+			results = lint.lintFiles(this.data[0], opts, opts.configFile),
+			failResultCount = lint.resultCount(results),
+			resultFormat = lint.format(results);
 
 
 		if (failResultCount > 0) {
-			grunt.fail.warn('');
-
-		} else {
 			grunt.log.writeln(resultFormat);
+			grunt.fail.warn('');
 		}
 	});
 };
