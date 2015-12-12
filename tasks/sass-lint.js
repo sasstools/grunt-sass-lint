a049b5503ef150da46c1d84562e9fda490af2486
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2015-12-12 20:23:15 +0100

    fail grunt build task on error lint

--- tasks/sass-lint.js
+++ tasks/sass-lint.js
@@ -1,17 +1,24 @@
 'use strict';
-var path = require('path');
-var lint = require('sass-lint');
+var path = require('path'),
+		lint = require('sass-lint');
 
 module.exports = function (grunt) {
 	grunt.verbose.writeln('\n' + lint.info + '\n');
 
 	grunt.registerMultiTask('sasslint', 'Lint your Sass', function () {
 		var opts = this.options({
-			configFile: ''
-		});
+					configFile: ''
+				}),
+				results = lint.lintFiles(this.data[0], opts, opts.configFile),
+				failResultCount = lint.resultCount(results),
+				resultFormat = lint.format(results);
 
-		var results = lint.lintFiles(this.data[0], opts, opts.configFile);
 
-		grunt.log.writeln(lint.format(results));
+		if (failResultCount > 0) {
+			grunt.fail.warn('');
+
+		} else {
+			grunt.log.writeln(resultFormat);
+		}
 	});
 };
