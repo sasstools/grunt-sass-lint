b24a7d9c2d098a951e57dd9d1dcdaf06b2bb1398
dutscher <dutscher_sbf@hotmail.com>
2016-07-29 12:16:55 +0200

    make the log output colorful
    via new option "logColor" and its color values http://slides.com/joshschumacher/grunt-logging#/5

--- tasks/sass-lint.js
+++ tasks/sass-lint.js
@@ -24,7 +24,7 @@ module.exports = function (grunt) {
 				opts['output-file'] = opts['outputFile'];
 				lint.outputResults(results, { options: opts });
 			} else {
-				grunt.log.writeln(resultFormat);
+				grunt.log.writeln(('logColor' in opts ? resultFormat[opts.logColor] : resultFormat));
 			}
       if (errorCount.count > 0) grunt.fail.warn('');
 		}
